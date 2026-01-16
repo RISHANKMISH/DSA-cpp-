2dafba291215e30aaf261dcd3acb9abccbd1fd98
RISHANKMISH <rishankm19@gmail.com>
2026-01-16 22:18:27 +0530

    Q2 added

--- Recursion.cpp
+++ Recursion.cpp
@@ -1,14 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void namePrint(int n){
 
-    cout<< "Rishank\n";
-}
+// // Q1)
 
-int main(){
-    string n;
-    cout<<"Enter value of n: ";
-    cin>> n;
-}
+// //  void namePrint(int n){
 
+// //     if (n==0) return;
+// //     cout<< "Rishank\n";
+
+// //     namePrint(n-1);
+// // }
+
+// // int main(){
+// //     int n;
+// //     cout<<"Enter value of n: ";
+// //     cin>> n;
+// //     namePrint(n);
+// // }
+
+// // Q2
+
+// void linearPrint(int n){
+
+//     if (n==0) return;
+
+//     cout <<n;
+
+//     linearPrint(n-1);
+
+// }
+
+
+
+// int main(){
+//     int n;
+//     cout<<"Enter value of n: ";
+//     cin>> n;
+//     linearPrint(n);
+// }
+
+
+
+// Q3)
